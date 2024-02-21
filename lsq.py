Fixed a bug where the cdf was multiplied by a factor due to scaling

lsq.py
@@ -228,18 +228,17 @@ class Mechanism:
                 assert len(domain_boundaries) == 2 
                 if domain_boundaries[0] is None or domain_boundaries[1] is None:
                     std = differential_privacy.compute_private_std(dataset, (private_min, private_max), epsilon)
-                self._x_lower_bound = private_min - std if domain_boundaries[0] is None else domain_boundaries[0]
-                self._x_upper_bound = private_max + std if domain_boundaries[1] is None else domain_boundaries[1]
+                self._x_lower_bound = private_min if domain_boundaries[0] is None else domain_boundaries[0]
+                self._x_upper_bound = private_max if domain_boundaries[1] is None else domain_boundaries[1]
             else:
                 std = differential_privacy.compute_private_std(dataset, (private_min, private_max), epsilon)
-                # factor 5 comes from the copulas module 
-                if self._x_lower_bound is None:
-                    self._x_lower_bound = private_min - std
-                if self._x_upper_bound is None:
-                    self._x_upper_bound = private_max + std
+                self._x_lower_bound = private_min
+                self._x_upper_bound = private_max
         else:
             self._x_lower_bound = dataset.min()
             self._x_upper_bound = dataset.max()
+        # check if compute lower and upper bound are reasonable
+        assert self._x_upper_bound > self._x_lower_bound
         self._x_range = self._x_upper_bound - self._x_lower_bound
         # normalize the dataset before fitting    
         dataset = dataset.copy()
@@ -261,7 +260,7 @@ class Mechanism:
             if self.sanitized:
                 self.mechanism_instance.sanitize(self.epsilon)
         # Now set it to the actual value
-        #self._pdf_multiplicator = 1.0 / self.compute_cdf_with_integral(self._x_upper_bound)
+        self._pdf_multiplicator = 1.0 / self.compute_cdf_with_integral(self._x_upper_bound)
     
     def compute_pdfs_for_queries(self, queries):
         if self.mechanism_name == 'SCIPY':
@@ -279,7 +278,7 @@ class Mechanism:
             pdf = lambda x: self.mechanism_instance.pdf(np.array([x]))
         else:
             pdf = lambda x: self.mechanism_instance.one_number_kde(x, self.sanitized)
-        return pdf((point - self._x_lower_bound) / self._x_range) * self._pdf_multiplicator
+        return pdf((point - self._x_lower_bound) / self._x_range) * (self._pdf_multiplicator / self._x_range)
         
     def compute_cdf_with_integral(self, end, start = None):
         # check if the boundaries need to be tightened
@@ -287,8 +286,9 @@ class Mechanism:
             start = self._x_lower_bound
         if end > self._x_upper_bound:
             end = self._x_upper_bound
-        start = 0 if start is None else start
-        return integrate.quad(self.compute_pdf, start, end, limit = 1000, epsabs = 10**-6)[0]
+        start = self._x_lower_bound if start is None else start
+        compute_pdf = lambda x: self.compute_pdf(x)
+        return integrate.quad(compute_pdf, start, end, limit = 1000, epsabs = 10**-6)[0]
     
     def inverse_cdf_with_integral(self, q: float):
         assert q >= 0 and q <= 1
@@ -365,3 +365,5 @@ def compare_mechanisms(mechanisms: list, domain_sizess: list, epsilons: list, n:
     else:
         comparison = pd.concat([comparison, comparison_for_continuous], axis = 0, ignore_index = True)
     return comparison
+
+# %%

main.py
@@ -1,7 +1,7 @@
 #%% Imports
 import numpy as np
 import pandas as pd
-from lsq import Mechanism, compare_mechanisms
+from lsq import GaussianKDE, Mechanism, compare_mechanisms, LSQ_FGT
 
 #%% Compare the mechanisms
 if __name__ == '__main__':
@@ -40,47 +40,28 @@ queries = np.random.choice(choices, (n_queries, dimension), p = probs)
 # paper config
 dataset = np.random.uniform(0, coordinate_range-1, (n_data, dimension))
 queries = np.random.uniform(0, coordinate_range-1, (n_queries, dimension))
-
-lsq_fgt_test =  Mechanism(mechanism_name = 'LSQ_FGT', sanitized = False, mechanism_parameters = {'rho': 4})
+queries = np.sort(queries)
+lsq_fgt_test =  Mechanism(mechanism_name = 'LSQ_FGT', sanitized = False, mechanism_parameters = {'rho': 4}, domain_boundaries=(0, 13))
 lsq_fgt_test.setup_mechanism(dataset)
-
-scipy_test = Mechanism(mechanism_name = "SCIPY")
-scipy_test.setup_mechanism(dataset)
-
-
-#%%
-
-def get_sqdistance_matrix(M1, M2):
-    allsqnorms = np.linalg.norm(np.vstack([M1,M2]), axis=1).reshape(-1, 1)**2
-    M1sqnorms = allsqnorms[:M1.shape[0],:]
-    M2sqnorms = allsqnorms[M1.shape[0]:,:].reshape(1, -1)
-    dm = M1sqnorms + M2sqnorms - 2.0 * np.dot(M1, M2.T)
-    dm[dm < 0.0] = 0.0
-    return dm
-
-### Exact KDE ###
-def GaussianKDE(dataset, queries):
-    exp_sq_dist_matrix = np.exp(-1 * get_sqdistance_matrix(dataset, queries))
-    return np.mean(exp_sq_dist_matrix, axis=0).T
-
-#%%
-min = np.floor(dataset.min())
-max = np.ceil(dataset.max())
 # Exact Gaussian KDE:
-exact_kde = scipy_test.compute_pdfs_for_queries(queries)
-exact_kde2 = GaussianKDE((dataset - min) / (max - min), (queries - min) / (max - min))
-exact_kde3 = GaussianKDE(dataset, queries)
-np.mean(np.abs(exact_kde - exact_kde3))
-
-#%%
+exact_kde = GaussianKDE(dataset, queries)
+# without my class
+mechanism = LSQ_FGT(dataset, dimension, coordinate_range, 4)
+non_dp_no_class = mechanism.non_private_kde(queries)
 # Non-DP KDE estimates:
 non_dp_kde_estimate = lsq_fgt_test.compute_pdfs_for_queries(queries)
-print("Non-DP estimate:", non_dp_kde_estimate)
 print("Mean error:", np.mean(np.abs(exact_kde - non_dp_kde_estimate)))
 # DP KDE estimates:
 lsq_fgt_test.setup_mechanism(dataset, epsilon = 0.1)
 dp_kde_estimate = lsq_fgt_test.compute_pdfs_for_queries(queries)
-print("DP estimate:", dp_kde_estimate)
 print("Mean error:", np.mean(np.abs(exact_kde - dp_kde_estimate)))
 
+#%%
+import matplotlib.pyplot as plt
+plt.plot(queries[:, 0], non_dp_no_class, label = "Non DP (No class)")
+plt.plot(queries[:, 0], exact_kde, label = "Exact")
+plt.plot(queries[:, 0], non_dp_kde_estimate, label = "Non DP")
+plt.plot(queries[:, 0], dp_kde_estimate, label = "DP")
+plt.legend()
+plt.show()
 # %%